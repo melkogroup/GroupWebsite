86e1c2056f403a795b7ebb5643a58fc79af93b75
rgmelko <rgmelko@uwaterloo.ca>
2013-05-14 09:50:57 -0400

    new file:   replace.py A small python script to search and replace sidebar items

---
+++ replace.py
@@ -0,0 +1,10 @@
+import os  
+import fileinput
+
+for root, dirs, files in os.walk('.'):
+    #for line in fileinput.FileInput("Lauren.html",inplace=1):
+    #for line in fileinput.input( (os.path.join(root,name) for name in files), inplace=True, backup='_bak'):
+    for line in fileinput.input( (os.path.join(root,name) for name in files), inplace=True):
+        line = line.replace("Code We've Built","Code We've Built")
+        line = line.replace("Awkward Group Photos","Awkward Group Photos")
+        print line,
