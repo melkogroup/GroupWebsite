d25c132fd2261f3d8c7d4fd69418c610d6c2f1c6
rgmelko <rgmelko@uwaterloo.ca>
2013-05-16 16:06:14 -0400

    Changed to postdoctoral research associate

--- replace.py
+++ replace.py
@@ -5,6 +5,7 @@ for root, dirs, files in os.walk('.'):
     #for line in fileinput.FileInput("Lauren.html",inplace=1):
     #for line in fileinput.input( (os.path.join(root,name) for name in files), inplace=True, backup='_bak'):
     for line in fileinput.input( (os.path.join(root,name) for name in files), inplace=True):
-        line = line.replace("Code We've Built","Code We've Built")
-        line = line.replace("Awkward Group Photos","Awkward Group Photos")
+        #line = line.replace("Code We've Built","Code We've Built")
+        #line = line.replace("Awkward Group Photos","Awkward Group Photos")
+        line = line.replace("postdoctoral research associates","postdoctoral research associates")
         print line,
