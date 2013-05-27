9c33331023f59a5c6a1ba70346dea5d2b3a88da3
rgmelko <rgmelko@uwaterloo.ca>
2013-05-27 16:03:55 -0400

    Changed the bottom menu item to link to dropbox

--- replace.py
+++ replace.py
@@ -7,5 +7,6 @@ for root, dirs, files in os.walk('.'):
     for line in fileinput.input( (os.path.join(root,name) for name in files), inplace=True):
         #line = line.replace("Code We've Built","Code We've Built")
         #line = line.replace("Awkward Group Photos","Awkward Group Photos")
-        line = line.replace("Computational quantum many-body research","Computational quantum many-body research")
+        #line = line.replace("Computational quantum many-body research","Computational quantum many-body research")
+        line = line.replace('<li><a href="https://www.dropbox.com/sh/kex0ui4f78ypzxd/PN641UnjIv">Online Material</a></li>','<li><a href="https://www.dropbox.com/sh/kex0ui4f78ypzxd/PN641UnjIv">Online Material</a></li>')
         print line,
